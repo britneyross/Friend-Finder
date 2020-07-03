cfa6b45f8fe5d5264555accade476c9395820c5e
Britney Ross <britneyross25@yahoo.com>
2020-07-02 23:03:54 -0500

    created home page and survey page

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+      name: "Mary",
+      photo: "https://media.gettyimages.com/photos/woman-analyzing-documents-while-sitting-at-home-picture-id1214479802?s=2048x2048",
+      scores: [
+        "3",
+        "2",
+        "5",
+        "1",
+        "3",
+        "4",
+        "3",
+        "4",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "John",
+      photo: "https://media.gettyimages.com/photos/portrait-of-smiling-young-man-wearing-eyeglasses-picture-id985138634?s=2048x2048",
+      scores: [
+        "3",
+        "5",
+        "4",
+        "1",
+        "2",
+        "4",
+        "2",
+        "5",
+        "2",
+        "2"
+      ]
+    },
+    {
+      name: "Tom",
+      photo: "https://media.gettyimages.com/photos/portrait-of-smiling-mid-adult-man-wearing-tshirt-picture-id985138674?s=2048x2048",
+      scores: [
+        "4",
+        "3",
+        "2",
+        "5",
+        "4",
+        "2",
+        "3",
+        "4",
+        "5",
+        "1"
+      ]
+    },
+    {
+      name: "Emily",
+      photo: "https://media.gettyimages.com/photos/young-woman-camper-picture-id509871550?s=2048x2048",
+      scores: [
+        "5",
+        "3",
+        "2",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "5",
+        "3"
+      ]
+    },
+    {
+      name: "Jessica",
+      photo: "https://media.gettyimages.com/photos/girl-painting-on-canvas-picture-id1125625188?s=2048x2048",
+      scores: [
+        "5",
+        "3",
+        "2",
+        "4",
+        "2",
+        "3",
+        "5",
+        "4",
+        "1",
+        "3"
+      ]
+    },
+    {
+      name: "Jason",
+      photo: "https://media.gettyimages.com/photos/business-video-conference-picture-id1184331589?s=2048x2048",
+      scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "4",
+        "1",
+        "3",
+        "4",
+        "1",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  module.exports = friends;
